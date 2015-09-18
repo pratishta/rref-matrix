bf4dcc1db20af05ca48d6b19d69119be3270c0ad
Pratishta Yerakala <ypradhi9@gmail.com>
2015-09-18 03:09:56 -0400

    got the algorithm to work and print each change along the way but doenst work for all matrices also there are negatives zeroes so needs formatting

--- rref-matrix/rref.py
+++ rref-matrix/rref.py
@@ -4,14 +4,29 @@ m = input("How many rows in your matrix? ")
 n = input("How many columns in your matrix? ")
 
 mat = []
-
 for i in range (0, m):
-        userin = raw_input("Type all the numbers in row %d separated by spaces: " % (i+1))
-        row = map(int, userin.split()) #split parses the input and separates by spaces; map converts them to integers
-        mat.append(row)
-        
-newmat = asarray(mat) #turns into a numpy array
+    userin = raw_input("Type all the numbers in row %d separated by spaces: " % (i+1))
+    row = map(float, userin.split()) # split parses the input and separates by spaces; map converts them to integers
+    mat.append(row)
 
+newmat = asarray(mat) # turns into a numpy array
+print newmat
+print "\n\n"
+lc = 0
+i = 0
 for i in range (0, m):
-        if (sum(newmat[i]) != 0):
-                print "they're not 0s"
+    if (sum(newmat[i]) != 0): # makes sure row isn't all 0s 
+        first = newmat[i] 
+        for j in range (0, n):
+            if (newmat[i][j] != 0):
+                lc = newmat[i][j]
+                print ("the leading coefficient is %d" %lc)
+                newmat[i] = first/(lc) # dividing the i'th row by the leading coefficient 
+                for k in range (0, m):
+                    if ((newmat[k][j] != 0) and (k!=i)):
+                        newmat[k] = newmat[k] - (newmat[i] * newmat[k][j])
+                print newmat
+                print "\n"
+                break  
+              
+print newmat
